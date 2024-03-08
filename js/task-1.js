128a0adc998e0bb32f67f889470af5ef4f68a03a
Valentyn-Melnyk <v.iv.melnyk@gmail.com>
2024-03-08 16:22:29 +0200

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,21 @@
 // 'use strict';
-function makeTransaction(quantity, pricePerDroid) { 
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${
+    quantity * pricePerDroid
+  } credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+function getLastElementMeta(array) {
+  const newArray = [];
+  newArray[0] = array.length;
+  newArray[1] = array[array.length - 1];
+  return newArray;
+}
+
+let ar = [];
+ar = getLastElementMeta(['apple', 'peach', 'pear', 'banana']);
+console.log(ar);
